e82a82b4c5a2047d0d5bb9ca98032a97f1671897
thephenom1708 <kmendki98@gmail.com>
2019-03-27 14:20:32 +0530

    new files added

--- principal/settings.py
+++ principal/settings.py
@@ -125,11 +125,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
