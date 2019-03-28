0d8a13b44def12e28014ea70443ef7591fa87653
thephenom1708 <kmendki98@gmail.com>
2019-03-28 19:01:17 +0530

    new files added

--- principal/settings.py
+++ principal/settings.py
@@ -125,14 +125,11 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, '/static/'),
+   os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
